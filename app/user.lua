639b0502ba7b801604c9d95f64c344022083d989
Gajos <thegajos@gmail.com>
2017-11-07 10:08:00 +0100

    Update user.lua

--- app/user.lua
+++ app/user.lua
@@ -1,1 +1,12 @@
+local ga = gajosframework
 
+ga.User = class(function(id)
+    self.id = id
+    
+    function self:GetName()
+      return player(id, "name")
+    end
+    function self:SetName(name, hide)
+      setname(name, hide or 0)
+    end
+end)
