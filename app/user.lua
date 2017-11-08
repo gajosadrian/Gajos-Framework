c46047b92767b1beff932b1ae22b92c3262017e6
Gajos <thegajos@gmail.com>
2017-11-08 09:01:42 +0100

    Update user.lua

--- app/user.lua
+++ app/user.lua
@@ -1,14 +1,38 @@
 local ga = gajosframework
 
 ga.User = classExtends(App, function(id)
-    self._id = id
+    self.id = id
     self._logic = ga.UserLogic.new(id)
     self._view = ga.UserView.new(id)
-    
+
     function self:GetName()
-      return player(id, "name")
+        return player(id, "name")
     end
     function self:SetName(name, hide)
-      setname(id, name, hide or 0)
+        setname(id, name, hide or 0)
     end
 end)
+
+ga.User.__index = function(self, key)
+    local id = self.id
+
+    if key == 'x' then
+        return player(id, "x")
+    elseif key == 'y' then
+        return player(id, "y")
+    else
+        return rawget(self, key)
+    end
+end
+
+ga.User.__newindex = function(self, key, value)
+    local id = self.id
+
+    if key == 'x' then
+        setpos(id, value, self.y)
+    elseif key == 'y' then
+        setpos(id, self.x, value)
+    else
+        rawset(self, key, value)
+    end
+end
