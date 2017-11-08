7bf4875beb39594ebd170f517a883ea1e1496d5f
Gajos <thegajos@gmail.com>
2017-11-08 14:39:16 +0100

    Update user.lua

--- app/user.lua
+++ app/user.lua
@@ -10,6 +10,46 @@ ga.User.__index = function(self, key)
     local id = self.id
 
     return switch(key) {
+        exists = function()
+            return player(id, 'exists')
+        end,
+
+        name = function()
+            return player(id, 'name')
+        end,
+
+        ip = function()
+            return player(id, 'ip')
+        end,
+
+        port = function()
+            return player(id, 'port')
+        end,
+
+        usgn = function()
+            return player(id, 'usgn')
+        end,
+
+        ping = function()
+            return player(id, 'ping')
+        end,
+
+        idle = function()
+            return player(id, 'idle')
+        end,
+
+        bot = function()
+            return player(id, 'bot')
+        end,
+
+        team = function()
+            return player(id, 'team')
+        end,
+
+        look = function()
+            return player(id, 'look')
+        end,
+
         x = function()
             return math.round(player(id, 'x'))
         end,
@@ -18,8 +58,128 @@ ga.User.__index = function(self, key)
             return math.round(player(id, 'y'))
         end,
 
-        name = function()
-            return player(id, 'name')
+        rot = function()
+            return player(id, 'rot')
+        end,
+
+        tilex = function()
+            return player(id, 'tilex')
+        end,
+
+        tiley = function()
+            return player(id, 'tiley')
+        end,
+
+        health = function()
+            return player(id, 'health')
+        end,
+
+        armor = function()
+            return player(id, 'armor')
+        end,
+
+        money = function()
+            return player(id, 'money')
+        end,
+
+        score = function()
+            return player(id, 'score')
+        end,
+
+        deaths = function()
+            return player(id, 'deaths')
+        end,
+
+        teamkills = function()
+            return player(id, 'teamkills')
+        end,
+
+        hostagekills = function()
+            return player(id, 'hostagekills')
+        end,
+
+        teambuildingkills = function()
+            return player(id, 'teambuildingkills')
+        end,
+
+        weapontype = function()
+            return player(id, 'weapontype')
+        end,
+
+        nightvision = function()
+            return player(id, 'nightvision')
+        end,
+
+        defusekit = function()
+            return player(id, 'defusekit')
+        end,
+
+        gasmask = function()
+            return player(id, 'gasmask')
+        end,
+
+        bomb = function()
+            return player(id, 'bomb')
+        end,
+
+        flag = function()
+            return player(id, 'flag')
+        end,
+
+        reloading = function()
+            return player(id, 'reloading')
+        end,
+
+        process = function()
+            return player(id, 'process')
+        end,
+
+        sprayname = function()
+            return player(id, 'sprayname')
+        end,
+
+        spraycolor = function()
+            return player(id, 'spraycolor')
+        end,
+
+        votekick = function()
+            return player(id, 'votekick')
+        end,
+
+        votemap = function()
+            return player(id, 'votemap')
+        end,
+
+        favteam = function()
+            return player(id, 'favteam')
+        end,
+
+        spectating = function()
+            return player(id, 'spectating')
+        end,
+
+        speedmod = function()
+            return player(id, 'speedmod')
+        end,
+
+        maxhealth = function()
+            return player(id, 'maxhealth')
+        end,
+
+        rcon = function()
+            return player(id, 'rcon')
+        end,
+
+        ai_flash = function()
+            return player(id, 'ai_flash')
+        end,
+
+        screenw = function()
+            return player(id, 'screenw')
+        end,
+
+        screenh = function()
+            return player(id, 'screenh')
         end,
 
         [Default] = function()
