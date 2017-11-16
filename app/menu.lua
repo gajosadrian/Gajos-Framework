3a2c234b2450c0c09f4b1c7bf416575305b780b0
Gajos <thegajos@gmail.com>
2017-11-16 22:52:34 +0100

    s

--- app/menu.lua
+++ app/menu.lua
@@ -4,13 +4,13 @@ ga.Menu = classExtends(App, function(id)
     self.id = id
     self._logic = ga.MenuLogic.new(id)
     self._view = ga.MenuView.new(id)
-	
-	function self:addButton(name, desc, visible)
-	end
-	
-	function self:setMenuType(type)
-	end
-	
-	function self:noskip()
-	end
+
+    function self:addButton(name, desc, visible)
+    end
+
+    function self:setMenuType(type)
+    end
+
+    function self:noskip()
+    end
 end
