d1bb93d9ca207acddbd19eb8c3699603d92fd77d
Gajos <thegajos@gmail.com>
2017-11-17 19:14:57 +0100

    Update menu.lua

--- controller/menu.lua
+++ controller/menu.lua
@@ -1,8 +1,8 @@
 local ga = gajosframework
 
-ga.Menu = classExtends(App, function(id)
+ga.Menu = classExtends(Controller, function(id)
     self.id = id
-    self._logic = ga.MenuLogic.new(id)
+    self._logic = ga.MenuModel.new(id)
     self._view = ga.MenuView.new(id)
 
     function self:addButton(name, desc, visible)
