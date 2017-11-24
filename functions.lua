2fa725dcdf5c46847fac4c905cfbc560878086cb
Gajos <thegajos@gmail.com>
2017-11-24 16:38:10 +0100

    bljat'

--- functions.lua
+++ functions.lua
@@ -1,3 +1,5 @@
+local ga = gajosframework
+
 function enumDir(dir)
     local t = {}
 
@@ -10,30 +12,89 @@ function enumDir(dir)
     return t
 end
 
-function msg3(id, message, _type, custom_tag)
-    local tag
-    if _type == 'warning' then
-        tag = COLOR.red
-    elseif _type == 'info' then
-        tag = COLOR.blue
-    elseif _type == 'alert' then
-        tag = COLOR.orange
-    elseif _type == 'success' then
-        tag = COLOR.light_green_accent
-    else
-        tag = '\169' .. VAR.server_color
-    end
+function getPath()
+    local str = debug.getinfo(2, 'S').source:sub(2)
+    return str:match('(.*/)')
+end
+
+function ga.getMessage(message, type, custom_tag)
+    local tag = switch() {
+        warning = function()
+            return COLOR.red
+        end,
+
+        info = function()
+            return COLOR.blue
+        end,
+
+        alert = function()
+            return COLOR.orange
+        end,
+
+        success = function()
+            return COLOR.light_green_accent
+        end,
+
+        [Default] = function()
+            '\169' .. VAR.server_color
+        end,
+    }
+
     tag = tag .. (custom_tag or VAR.server_tag)
     local txt = tag .. ': ' .. COLOR.white
 
     message = message:gsub('[>][>]', COLOR.red_lighten)
     message = message:gsub('[<][<]', COLOR.white)
 
-    if not id or id == 0 then
-        msg(txt .. message)
-    elseif id == -1 then
-        return txt .. message
-    else
-        msg2(id, txt .. message)
+    return txt .. message
+end
+
+_dofile = dofile
+function dofile(path, create)
+    if not io.exists(path) then
+        if create then
+            local file = io.open(path, 'w')
+            io.close(file)
+
+            print2('The file "'..path..'" couldn\'t be found or opened, creating new one...', 'notice')
+        else
+            print2('The file "'..path..'" couldn\'t be found or opened!', 'error')
+        end
+
+        return false
     end
+
+    _dofile(path)
+    return true
+end
+
+function io.exists(path)
+    local file = io.open(path, 'r')
+
+    if file then
+        io.close(file)
+        return true
+    else
+        return false
+    end
+end
+
+_msg = msg
+function msg(text, type, prefix)
+    message = ga.getMessage(text, type, prefix)
+    _msg(message)
+end
+
+_msg2 = msg2
+function msg2(id, text, type, prefix)
+    message = ga.getMessage(text, type, prefix)
+    _msg2(id, message)
+end
+
+function print2(text, type, prefix)
+    message = ga.getMessage(text, type, prefix)
+    print(message)
+end
+
+function log(message)
 end
--- main.lua
+++ main.lua
@@ -1,1 +1,3 @@
+local _DIR = getPath()
 
+print(getPath())
