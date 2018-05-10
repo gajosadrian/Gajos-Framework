bf8f3858023c806cf2b8a99c0abca9b02ecc8c3f
Gajos <thegajos@gmail.com>
2018-05-10 16:48:24 +0200

    Update class.lua

--- lib/class.lua
+++ lib/class.lua
@@ -17,38 +17,69 @@ function Class(namespace,constructor)
     end
 end
 
-function class(constructor)
-    namespace = {}
-    namespace.__index = namespace
-    namespace.new = function(...)
-        local outerSelf = self
-        -- aliases
-        local this = {}
-        self = this
-        -- finish
-        setmetatable(this,namespace)
-        constructor(unpack(arg))
-        self = outerSelf -- used to allow constructors inside constructors
-        return this
+local function tableContains(table, element)
+  for _, value in pairs(table) do
+    if value == element then
+      return true
     end
-    return namespace
+  end
+  return false
 end
 
-function classExtends(extend,constructor)
-    namespace = {}
-    namespace.__index = namespace
-    namespace.new = function(...)
-        local outerSelf = self
-        -- aliases
-        local this = extend.new()
-        self = this
-        -- finish
-        --setmetatable(this,namespace)
-        constructor(unpack(arg))
-        self = outerSelf -- used to allow constructors inside constructors
-        return this
+function class(constructor)
+  local namespace = {}
+  namespace.__index = namespace
+  namespace.new = function(...)
+    local outerSelf = self
+    -- aliases
+    local this = {}
+    self = this
+    -- metatable
+    setmetatable(this, namespace)
+    constructor(namespace)
+    -- constructor
+    if this.__construct then
+      this:__construct(unpack(arg))
     end
-    return namespace
+    -- finish
+    self = outerSelf -- used to allow constructors inside constructors
+    return this
+  end
+  return namespace
+end
+
+function classExtends(extend, constructor)
+  namespace = {}
+  namespace.__index = namespace
+  namespace.new = function(...)
+    local outerSelf = self
+    -- aliases
+    local this = extend.new()
+    self = this
+    -- metatable
+    -- setmetatable(this,namespace)
+    constructor(namespace)
+    -- copying statics
+    local notAllowedVarNames = {'__index', '__newindex'}
+    for varName, varValue in pairs(namespace) do
+      if not tableContains(notAllowedVarNames, varName) then
+        extend[varName] = varValue
+      end
+    end
+    for varName, varValue in pairs(extend) do
+      if not tableContains(notAllowedVarNames, varName) then
+        namespace[varName] = varValue
+      end
+    end
+    -- constructor
+    if this.__construct then
+      this:__construct(unpack(arg))
+    end
+    -- finish
+    self = outerSelf -- used to allow constructors inside constructors
+    return this
+  end
+  return namespace
 end
 
 abstract_class = class
@@ -56,12 +87,15 @@ abstract_class = class
 -- how to
 
 --[[
-classname = {}
-Class(classname,function(my_arguments)
-    self.something = my_arguments
-end) -- do not return
+classname = class(function(static)
+  static.SOMETHING = false
 
-function classname:method()
+  function self:__construct(my_arguments)
+    self.something = my_arguments
+  end
+
+  function self:method()
     print(this)
-end
+  end
+end) -- do not return
 ]]--
